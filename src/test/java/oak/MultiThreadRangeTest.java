119027b36655ad4e18e7e68f68a3c3c825749ad9
anastas <anastas@yahoo-inc.com>
2018-06-27 11:55:56 +0300

    Adding license headers to the test files

--- src/test/java/oak/MultiThreadRangeTest.java
+++ src/test/java/oak/MultiThreadRangeTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2018 Oath Inc.
+ * Licensed under the terms of the Apache 2.0 license.
+ * Please see LICENSE file in the project root for terms.
+ */
+
 package oak;
 
 import org.junit.Before;
