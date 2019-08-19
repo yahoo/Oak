9e35ef95e73266ad6ca4df149eba233145619e2a
yoavz1997 <yoavzuriel@cs.technion.ac.il>
2019-08-19 15:11:59 +0300

    test resizing put

--- core/src/test/java/com/oath/oak/ResizeValueTest.java
+++ core/src/test/java/com/oath/oak/ResizeValueTest.java
@@ -1,4 +1,41 @@
 package com.oath.oak;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ResizeValueTest {
+    private OakMap<String, String> oak;
+
+    @Before
+    public void initStuff() {
+        OakMapBuilder<String, String> builder = new OakMapBuilder<String, String>()
+                .setChunkMaxItems(100)
+                .setChunkBytesPerItem(128)
+                .setKeySerializer(new StringSerializer())
+                .setValueSerializer(new StringSerializer())
+                .setComparator(new StringComparator())
+                .setMinKey("");
+
+        oak = builder.build();
+    }
+
+    @Test
+    public void testMain() {
+        String key = "Hello";
+        String value = "h";
+        oak.zc().put(key, value);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            stringBuilder.append(i);
+        }
+        OakRBuffer valBuffer = oak.zc().get(key);
+        String transformed = valBuffer.transform(b -> new StringSerializer().deserialize(b));
+        assertEquals(value, transformed);
+        oak.zc().put(key, stringBuilder.toString());
+        valBuffer = oak.zc().get(key);
+        transformed = valBuffer.transform(b -> new StringSerializer().deserialize(b));
+        assertEquals(stringBuilder.toString(), transformed);
+    }
 }
