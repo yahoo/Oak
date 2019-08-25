d6118115bc17c7f8ab1e7e601ed25060168c9d2e
yoavz1997 <yoavzuriel@cs.technion.ac.il>
2019-08-25 16:45:23 +0300

    added a new test

--- core/src/test/java/com/oath/oak/ChunkSplitTest.java
+++ core/src/test/java/com/oath/oak/ChunkSplitTest.java
@@ -2,26 +2,31 @@ package com.oath.oak;
 
 
 import org.junit.Test;
+
 import java.lang.reflect.Field;
 import java.util.concurrent.ConcurrentSkipListMap;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ChunkSplitTest {
+    private static final int maxItemsPerChunk = 10;
 
     @Test
     public void testSplitByCount() throws NoSuchFieldException, IllegalAccessException {
         OakMapBuilder<String, String> builder = new OakMapBuilder<String, String>()
-                .setChunkMaxItems(10)
+                .setChunkMaxItems(maxItemsPerChunk)
                 .setChunkBytesPerItem(1024)
                 .setKeySerializer(new StringSerializer())
                 .setValueSerializer(new StringSerializer())
                 .setComparator(new StringComparator())
                 .setMinKey("");
-        OakMap<String, String> oak =  builder.build();
+        OakMap<String, String> oak = builder.build();
 
-        for (int i=0; i < 11; i++) {
+        for (int i = 0; i < maxItemsPerChunk + 1; i++) {
             String key = String.format("-%01d", i);
-            oak.zc().put(key,key);
+            oak.zc().put(key, key);
         }
 
         Field field = oak.getClass().getDeclaredField("internalOakMap");
@@ -33,6 +38,62 @@ public class ChunkSplitTest {
         ConcurrentSkipListMap<Object, Chunk<String, String>> skipList = (ConcurrentSkipListMap<Object, Chunk<String, String>>) skipListField.get(internalOakMap);
         assertTrue(skipList.size() > 1);
         oak.close();
+    }
+
+    @Test
+    public void testSplitByGet() {
+        OakMapBuilder<Integer, Integer> builder = OakMapBuilder.getDefaultBuilder()
+                .setChunkMaxItems(maxItemsPerChunk)
+                .setChunkBytesPerItem(1024);
+        OakMap<Integer, Integer> oak = builder.build();
+        Integer value;
+        for (Integer i = 0; i < 2 * maxItemsPerChunk; i++) {
+            oak.zc().put(i, i);
+        }
+        for (Integer i = 0; i < 2 * maxItemsPerChunk; i++) {
+            value = oak.get(i);
+            assertEquals(i, value);
+        }
+
+        for (Integer i = 0; i < maxItemsPerChunk; i++) {
+            oak.zc().remove(i);
+        }
+        for (Integer i = 0; i < maxItemsPerChunk; i++) {
+            value = oak.get(i);
+            assertNull(value);
+        }
+        for (Integer i = maxItemsPerChunk; i < 2 * maxItemsPerChunk; i++) {
+            value = oak.get(i);
+            assertEquals(i, value);
+        }
+
+        for (Integer i = maxItemsPerChunk; i < 2 * maxItemsPerChunk; i++) {
+            oak.zc().remove(i);
+        }
+        for (Integer i = maxItemsPerChunk; i < 2 * maxItemsPerChunk; i++) {
+            value = oak.get(i);
+            assertNull(value);
+        }
+        for (int i = 0; i < 2 * maxItemsPerChunk; i++) {
+            oak.zc().put(i, i);
+            value = oak.get(0);
+            assertEquals(0, value.intValue());
+        }
+        for (int i = 0; i < 2 * maxItemsPerChunk; i++) {
+            value = oak.get(i);
+            assertEquals(i, value.intValue());
+        }
+
+
+        for (Integer i = 1; i < (2 * maxItemsPerChunk - 1); i++) {
+            oak.zc().remove(i);
+        }
+
+        value = oak.get(0);
+        assertEquals(0, value.intValue());
+
+        value = oak.get(2 * maxItemsPerChunk - 1);
+        assertEquals(2 * maxItemsPerChunk - 1, value.intValue());
     }
 
     // the size of the keys is no longer the requirement for the split
