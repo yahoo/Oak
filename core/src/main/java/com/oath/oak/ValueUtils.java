f15f760a0440f1f1a9d69f96bee910f29df41726
yoavz1997 <yoavzuriel@cs.technion.ac.il>
2019-08-27 09:46:05 +0300

    a value can be moved, and the return value includes RETRY

--- core/src/main/java/com/oath/oak/ValueUtils.java
+++ core/src/main/java/com/oath/oak/ValueUtils.java
@@ -6,15 +6,32 @@ import sun.nio.ch.DirectBuffer;
 import java.lang.reflect.Constructor;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.AbstractMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import static com.oath.oak.Chunk.VALUE_BLOCK_SHIFT;
+import static com.oath.oak.Chunk.VALUE_LENGTH_MASK;
+import static com.oath.oak.ValueUtils.LockStats.*;
+import static com.oath.oak.ValueUtils.ValueResult.*;
+
 public class ValueUtils {
 
+    enum LockStats {
+        FREE(0), LOCKED(1), DELETED(2), MOVED(3);
+
+        public int value;
+
+        LockStats(int value) {
+            this.value = value;
+        }
+    }
+
+    enum ValueResult {
+        SUCCESS, FAILURE, RETRY
+    }
+
     private static final int LOCK_MASK = 0x3;
-    private static final int LOCK_FREE = 0;
-    private static final int LOCK_LOCKED = 1;
-    private static final int LOCK_DELETED = 2;
     public static final int VALUE_HEADER_SIZE = 4;
 
     private static Unsafe unsafe;
@@ -67,22 +84,27 @@ public class ValueUtils {
     }
 
     private static boolean isValueDeleted(int header) {
-        return header == LOCK_DELETED;
+        return header == DELETED.value;
     }
 
-    static boolean lockRead(Slice s) {
+    private static boolean wasValueMoved(int header) {
+        return header == MOVED.value;
+    }
+
+    static ValueResult lockRead(Slice s) {
         return lockRead(s.getByteBuffer());
     }
 
-    static boolean lockRead(ByteBuffer bb) {
+    static ValueResult lockRead(ByteBuffer bb) {
         assert bb.isDirect();
         int oldHeader;
         do {
             oldHeader = bb.getInt(bb.position());
-            if (isValueDeleted(oldHeader)) return false;
+            if (isValueDeleted(oldHeader)) return FAILURE;
+            if (wasValueMoved(oldHeader)) return RETRY;
             oldHeader &= ~LOCK_MASK;
         } while (!CAS(bb, oldHeader, oldHeader + 4));
-        return true;
+        return SUCCESS;
     }
 
     static void unlockRead(Slice s) {
@@ -97,29 +119,31 @@ public class ValueUtils {
         } while (!CAS(bb, oldHeader, oldHeader - 4));
     }
 
-    private static boolean lockWrite(ByteBuffer bb) {
+    private static ValueResult lockWrite(ByteBuffer bb) {
         assert bb.isDirect();
         int oldHeader;
         do {
             oldHeader = bb.getInt(bb.position());
-            if (isValueDeleted(oldHeader)) return false;
-        } while (!CAS(bb, LOCK_FREE, LOCK_LOCKED));
-        return true;
+            if (isValueDeleted(oldHeader)) return FAILURE;
+            if (wasValueMoved(oldHeader)) return RETRY;
+        } while (!CAS(bb, FREE.value, LOCKED.value));
+        return SUCCESS;
     }
 
     private static void unlockWrite(ByteBuffer bb) {
-        bb.putInt(bb.position(), LOCK_FREE);
+        bb.putInt(bb.position(), FREE.value);
         // maybe a fence?
     }
 
-    private static boolean deleteValue(ByteBuffer bb) {
+    private static ValueResult deleteValue(ByteBuffer bb) {
         assert bb.isDirect();
         int oldHeader;
         do {
             oldHeader = bb.getInt(bb.position());
-            if (isValueDeleted(oldHeader)) return false;
-        } while (!CAS(bb, LOCK_FREE, LOCK_DELETED));
-        return true;
+            if (isValueDeleted(oldHeader)) return FAILURE;
+            if (wasValueMoved(oldHeader)) return RETRY;
+        } while (!CAS(bb, FREE.value, DELETED.value));
+        return SUCCESS;
     }
 
     /* Handle Methods */
@@ -129,60 +153,70 @@ public class ValueUtils {
     }
 
     static <T> T transform(ByteBuffer bb, Function<ByteBuffer, T> transformer) {
-        if (!lockRead(bb)) return null;
+        if (lockRead(bb) != SUCCESS) return null;
 
         T transformation = transformer.apply(getActualValueBuffer(bb).asReadOnlyBuffer());
         unlockRead(bb);
         return transformation;
     }
 
-    static <T> T transform(Slice s, Function<ByteBuffer, T> transformer) {
+    static <T> AbstractMap.SimpleEntry<ValueResult, T> transform(Slice s, Function<ByteBuffer, T> transformer) {
         ByteBuffer bb = s.getByteBuffer();
-        if (!lockRead(bb)) return null;
+        ValueResult res = lockRead(bb);
+        if (res != SUCCESS) return new AbstractMap.SimpleEntry<>(res, null);
 
         T transformation = transformer.apply(getActualValueBuffer(bb).asReadOnlyBuffer());
         unlockRead(bb);
-        return transformation;
+        return new AbstractMap.SimpleEntry<>(SUCCESS, transformation);
     }
 
-    public static <V> boolean put(Slice s, V newVal, OakSerializer<V> serializer, MemoryManager memoryManager) {
-        ByteBuffer bb = s.getByteBuffer();
-        if (!lockWrite(bb)) return false;
+    public static <K, V> ValueResult put(Chunk<K, V> chunk, Chunk.LookUp lookUp, V newVal, OakSerializer<V> serializer, MemoryManager memoryManager) {
+        ByteBuffer bb = lookUp.valueSlice.getByteBuffer();
+        ValueResult res = lockWrite(bb);
+        if (res != SUCCESS) return res;
         int capacity = serializer.calculateSize(newVal);
         if (bb.remaining() < capacity) { // can not reuse the existing space
-            memoryManager.releaseSlice(s);
+            bb.putInt(bb.position(), MOVED.value);
+            ByteBuffer dup = bb.duplicate();
+            dup.position(dup.position() + 4);
+            Slice s = lookUp.valueSlice;
+            Slice sDup = new Slice(s.getBlockID(), dup);
+            memoryManager.releaseSlice(sDup);
             s = memoryManager.allocateSlice(capacity + VALUE_HEADER_SIZE);
             bb = s.getByteBuffer();
-            throw new UnsupportedOperationException();
-            // TODO: update the relevant entry
+            bb.putInt(bb.position(), LOCKED.value);
+            int valueBlockAndLength = (s.getBlockID() << VALUE_BLOCK_SHIFT) | ((capacity + VALUE_HEADER_SIZE) & VALUE_LENGTH_MASK);
+            assert chunk.longCasEntriesArray(lookUp.entryIndex, Chunk.OFFSET.VALUE_STATS, lookUp.valueStats, UnsafeUtils.intsToLong(valueBlockAndLength, bb.position()));
+            // TODO: CAS the new slice into the entry
         }
         ByteBuffer dup = getActualValueBuffer(bb);
         // It seems like I have to change bb here or else I expose the header to the user
         // Duplicating bb instead
         serializer.serialize(newVal, dup);
         unlockWrite(bb);
-        return true;
+        return SUCCESS;
     }
 
-    // Why do we use finally here and not in put for example
-    static boolean compute(Slice s, Consumer<OakWBuffer> computer) {
+    static ValueResult compute(Slice s, Consumer<OakWBuffer> computer) {
         ByteBuffer bb = s.getByteBuffer();
-        if (!lockWrite(bb)) return false;
+        ValueResult res = lockWrite(bb);
+        if (res != SUCCESS) return res;
         OakWBuffer wBuffer = new OakWBufferImpl(bb);
         computer.accept(wBuffer);
         unlockWrite(bb);
-        return true;
+        return SUCCESS;
     }
 
-    static boolean remove(Slice s, MemoryManager memoryManager) {
+    static ValueResult remove(Slice s, MemoryManager memoryManager) {
         ByteBuffer bb = s.getByteBuffer();
-        if (!deleteValue(bb)) return false;
+        ValueResult res = deleteValue(bb);
+        if (res != SUCCESS) return res;
         // releasing the actual value and not the header
         ByteBuffer dup = bb.duplicate();
         dup.position(dup.position() + 4);
         Slice sDup = new Slice(s.getBlockID(), dup);
         memoryManager.releaseSlice(sDup);
-        return true;
+        return SUCCESS;
     }
 
     /**
@@ -194,7 +228,7 @@ public class ValueUtils {
     static <T> T mutatingTransform(Slice s, Function<ByteBuffer, T> transformer) {
         T result;
         ByteBuffer bb = s.getByteBuffer();
-        if (!lockWrite(bb))
+        if (lockWrite(bb) != SUCCESS)
             // finally clause will handle unlock
             return null;
         result = transformer.apply(getActualValueBuffer(bb));
