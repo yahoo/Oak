e704c8ed97a2dd50573e56d3bb3159fce01154e6
anastas <anastas@yahoo-inc.com>
2021-07-22 12:37:49 +0300

    small continuation with FirstLevelHashArray

--- core/src/main/java/com/yahoo/oak/FirstLevelHashArray.java
+++ core/src/main/java/com/yahoo/oak/FirstLevelHashArray.java
@@ -4,8 +4,18 @@ package com.yahoo.oak;
 class FirstLevelHashArray<K,V> {
 
   private final HashChunk<K, V>[] chunks;    // array is initialized to 0 - this is important!
+  private final int msbForFirstLevelHash;
+
 
   FirstLevelHashArray(int msbForFirstLevelHash) {
     this.chunks = new HashChunk[msbForFirstLevelHash];
+    this.msbForFirstLevelHash = msbForFirstLevelHash;
+
+    // least significant bits remaining
+    int lsbForSecondLEvel = Integer.SIZE - msbForFirstLevelHash;
+
+    // size of HashChunk should be 2^lsbForSecondLEvel
+    int chunkSize = (int) (Math.pow(2, lsbForSecondLEvel) + 1);
+
   }
 }
