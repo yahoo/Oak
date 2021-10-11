f953c8b7df0df42e53d82a965c96818f69d31d7c
Liran Funaro <liran.funaro@verizonmedia.com>
2021-10-11 15:39:00 +0300

    Increase murmur test coverage

--- core/src/test/java/com/yahoo/oak/MurmurTest.java
+++ core/src/test/java/com/yahoo/oak/MurmurTest.java
@@ -13,10 +13,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class MurmurTest {
 
     private OakMap<byte[], byte[]> oak;
+
+    private static final int BYTE_ARRAY_SIZE = 10;
+    private static final int NUM_ITERATIONS = 100;
 
     @Before
     public void init() {
@@ -30,42 +34,54 @@ public class MurmurTest {
 
     @Test
     public void test32() {
-        final byte[] test = new byte[] {1, 2, 3};
-        final int expected = MurmurHash3.murmurhash32(test, 0, test.length, 0);
-        oak.put(test, test);
+        Random rand = new Random();
 
-        OakBuffer oakBuffer = oak.zc().get(test);
-        Assert.assertNotNull(oakBuffer);
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            final byte[] test = new byte[BYTE_ARRAY_SIZE];
+            rand.nextBytes(test);
 
-        Assert.assertEquals(test.length, oakBuffer.capacity() - Integer.BYTES);
+            final int expected = MurmurHash3.murmurhash32(test, 0, test.length, 0);
+            oak.put(test, test);
 
-        final int actual = MurmurHash3.murmurhash32(oakBuffer, Integer.BYTES, test.length, 0);
-        Assert.assertEquals(expected, actual);
+            OakBuffer oakBuffer = oak.zc().get(test);
+            Assert.assertNotNull(oakBuffer);
 
-        ByteBuffer byteBuffer = ((OakUnsafeDirectBuffer) oakBuffer).getByteBuffer();
-        final int actualByteBuffer = MurmurHash3.murmurhash32(byteBuffer, Integer.BYTES, test.length, 0);
-        Assert.assertEquals(expected, actualByteBuffer);
+            Assert.assertEquals(test.length, oakBuffer.capacity() - Integer.BYTES);
+
+            final int actual = MurmurHash3.murmurhash32(oakBuffer, Integer.BYTES, test.length, 0);
+            Assert.assertEquals(expected, actual);
+
+            ByteBuffer byteBuffer = ((OakUnsafeDirectBuffer) oakBuffer).getByteBuffer();
+            final int actualByteBuffer = MurmurHash3.murmurhash32(byteBuffer, Integer.BYTES, test.length, 0);
+            Assert.assertEquals(expected, actualByteBuffer);
+        }
     }
 
     @Test
     public void test128() {
-        final byte[] test = new byte[] {1, 2, 3};
-        final MurmurHash3.HashCode128 expected = new MurmurHash3.HashCode128();
-        MurmurHash3.murmurhash128(test, 0, test.length, 0, expected);
-        oak.put(test, test);
+        Random rand = new Random();
 
-        OakBuffer oakBuffer = oak.zc().get(test);
-        Assert.assertNotNull(oakBuffer);
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            final byte[] test = new byte[BYTE_ARRAY_SIZE];
+            rand.nextBytes(test);
 
-        Assert.assertEquals(test.length, oakBuffer.capacity() - Integer.BYTES);
+            final MurmurHash3.HashCode128 expected = new MurmurHash3.HashCode128();
+            MurmurHash3.murmurhash128(test, 0, test.length, 0, expected);
+            oak.put(test, test);
 
-        final MurmurHash3.HashCode128 actual = new MurmurHash3.HashCode128();
-        MurmurHash3.murmurhash128(oakBuffer, Integer.BYTES, test.length, 0, actual);
-        Assert.assertEquals(expected, actual);
+            OakBuffer oakBuffer = oak.zc().get(test);
+            Assert.assertNotNull(oakBuffer);
 
-        final MurmurHash3.HashCode128 actualByteBuffer = new MurmurHash3.HashCode128();
-        ByteBuffer byteBuffer = ((OakUnsafeDirectBuffer) oakBuffer).getByteBuffer();
-        MurmurHash3.murmurhash128(byteBuffer, Integer.BYTES, test.length, 0, actualByteBuffer);
-        Assert.assertEquals(expected, actualByteBuffer);
+            Assert.assertEquals(test.length, oakBuffer.capacity() - Integer.BYTES);
+
+            final MurmurHash3.HashCode128 actual = new MurmurHash3.HashCode128();
+            MurmurHash3.murmurhash128(oakBuffer, Integer.BYTES, test.length, 0, actual);
+            Assert.assertEquals(expected, actual);
+
+            final MurmurHash3.HashCode128 actualByteBuffer = new MurmurHash3.HashCode128();
+            ByteBuffer byteBuffer = ((OakUnsafeDirectBuffer) oakBuffer).getByteBuffer();
+            MurmurHash3.murmurhash128(byteBuffer, Integer.BYTES, test.length, 0, actualByteBuffer);
+            Assert.assertEquals(expected, actualByteBuffer);
+        }
     }
 }
